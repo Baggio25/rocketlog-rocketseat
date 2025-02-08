c4d497b29b7c29c4f5179ff5f1e0a0e1ba9a8432
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-02-07 22:23:27 -0300

    Cadastrando usuário no bd

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -3,6 +3,9 @@ import { Request, Response } from "express";
 import { hash } from "bcrypt";
 import { z } from "zod";
 
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
+
 class UsersController {
 
   async create(request: Request, response: Response) {
@@ -14,9 +17,25 @@ class UsersController {
 
     const { name, email, password } = bodySchema.parse(request.body);
 
+    const userWithSameEmail = await prisma.user.findFirst({ where: { email }});
+
+    if(userWithSameEmail) {
+      throw new AppError("User with same email already exist");
+    }
+
     const hashedPassword = await hash(password, 8);
 
-    return response.json({ message: "create user, password", hashedPassword});
+    const user = await prisma.user.create({ 
+      data: {
+        name,
+        email,
+        password: hashedPassword
+      }
+     });
+
+    const { password: _, ...userWithoutPassword } = user;
+
+    return response.json(userWithoutPassword);
   }
 
 }
