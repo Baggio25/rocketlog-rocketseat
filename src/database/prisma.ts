7363290989032d8b641267108a0e99d84253d538
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-02-09 22:04:00 -0300

    Validando Variável de Ambiente

--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -1,6 +1,7 @@
+import { env } from "@/env";
 import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient({
-  log: process.env.NODE_ENV === "production" ? [] : ["query"]
+  log: env.NODE_ENV === "production" ? [] : ["query"]
 });
 
